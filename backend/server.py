b0d9a6249d63b351567b8451b63954de421d52e8
Jscastro197 <overbeatzme@gmail.com>
2023-09-26 18:20:19 -0400

    testing signin

--- backend/server.py
+++ backend/server.py
@@ -111,8 +111,11 @@ def signup():
 
 @app.route('/api/token')
 def token():
-    email = request.form.get('email')
-    password = request.form.get('password')
+    data = request.get_json()
+    # email = request.form.get('email')
+    # password = request.form.get('password')
+    email = data.get('email')
+    password = data.get('password')
     try:
         user = pb.auth().sign_in_with_email_and_password(email, password)
         jwt = user['idToken']
