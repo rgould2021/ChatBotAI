f234c02e2c48bca868f23bf26ef5ea2577e41545
Jeff Castro <133550490+Jscastro197@users.noreply.github.com>
2023-10-21 20:41:41 -0400

    login?

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StatusBar, View } from 'react-native';
-import ChatScreen from '../../screens/ChatScreen';
 import LoginScreen from '../../screens/LoginScreen';
 
 const App: React.FC = () => {
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="dark-content" />
-      <ChatScreen />
+      <LoginScreen/>
     </View>
   );
 };
