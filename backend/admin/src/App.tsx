8be8f517b82db0b5e25de0e257f8b7b9195161a9
Jeff Castro <133550490+Jscastro197@users.noreply.github.com>
2023-12-01 19:11:00 -0500

    admin page skeleton

---
+++ backend/admin/src/App.tsx
@@ -0,0 +1,13 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}> <Resource name="users" list={ListGuesser} /> </Admin>
+);
