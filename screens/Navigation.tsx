069efaa96999de07f4df586135e35fc813b4b479
Kenny Demos <kdemsthene2018@fau.edu>
2023-12-05 00:12:41 -0500

    add logout function

--- screens/Navigation.tsx
+++ screens/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
@@ -8,22 +8,70 @@ import Home from './Home';
 import UpdateProfile from './UpdateProfile';
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
+import { Button, View } from 'react-native';
+import auth from '@react-native-firebase/auth';
  
 
 const Stack = createNativeStackNavigator();
 
 
 const navigation = () => {
+
+  const navigation = useNavigation();
+
+  const Logout = async () => {  
+      try {
+          await auth().signOut();
+          navigation.navigate('LoginScreen');  
+      } catch (error) {
+          console.error('Error logging out:', error);
+      }
+  };
   return (
     // <NavigationContainer>
       <Stack.Navigator initialRouteName ="LoginScreen">
-        <Stack.Screen options={{ title: ''}}name="LoginScreen"  component = {LoginScreen} />
-        <Stack.Screen options={{ title: ''}}name="SignupScreen" component = {SignupScreen} /> 
-        <Stack.Screen options={{ title: ''}}name="ChatScreen" component = {ChatScreen} /> 
-        <Stack.Screen options={{ title: ''}}name="Home" component = {Home} /> 
-        <Stack.Screen options={{ title: ''}}name="UpdateProfile" component = {UpdateProfile} /> 
-        <Stack.Screen options={{ title: ''}}name="AboutUs" component = {AboutUs} /> 
-        <Stack.Screen options={{ title: ''}} name = "ContactUs" component = {ContactUs} />
+        <Stack.Screen options={{ title: 'Login'}}name="LoginScreen"  component = {LoginScreen} />
+        <Stack.Screen options={{ title: 'Sign Up'}}name="SignupScreen" component = {SignupScreen} /> 
+        <Stack.Screen options={{ title: 'Chat Bot', headerRight: () => (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10 }}>
+          <Button
+            title="Logout"
+            onPress={ Logout}
+          />
+        </View>
+         ),}}name="ChatScreen" component = {ChatScreen} /> 
+        <Stack.Screen options={{ title: 'Home',headerLeft: null,  headerRight: () => (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10 }}>
+          <Button
+            title="Logout"
+            onPress={ Logout}
+          />
+        </View>
+         ),}}name="Home" component = {Home} /> 
+        <Stack.Screen options={{ title: 'Profile', headerRight: () => (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10 }}>
+          <Button
+            title="Logout"
+            onPress={ Logout}
+          />
+        </View>
+         ),}}name="UpdateProfile" component = {UpdateProfile} /> 
+        <Stack.Screen options={{ title: 'About Us', headerRight: () => (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10 }}>
+          <Button
+            title="Logout"
+            onPress={ Logout}
+          />
+        </View>
+         ),}}name="AboutUs" component = {AboutUs} /> 
+        <Stack.Screen options={{ title: 'Contact Us', headerRight: () => (
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginRight: 10 }}>
+          <Button
+            title="Logout"
+            onPress={ Logout}
+          />
+        </View>
+         ),}} name = "ContactUs" component = {ContactUs} />
       </Stack.Navigator>
     // </NavigationContainer>
   );
